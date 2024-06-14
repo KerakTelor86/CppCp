33dbe8806caa9a105f1183b3f3be7879dc8e4ab0
Richard Alison <richardalison86@gmail.com>
2024-06-15 03:30:31 +0900

    Add (running) fold and reduce to functional header

--- func.hpp
+++ func.hpp
@@ -2,6 +2,7 @@
 #define CPPCP_FUNCTIONAL
 
 #include <array>
+#include <numeric>
 #include <vector>
 
 #include "concepts.hpp"
@@ -19,6 +20,43 @@ template <typename Func> struct Filter {
 struct FilterBuilder {
     auto operator()(const auto& func) const {
         return Filter(func);
+    }
+};
+
+template <typename Init, typename Func> struct Fold {
+    Fold(const Init& _init, const Func& _func) : init(_init), func(_func) {}
+    const Init init;
+    const Func func;
+};
+
+struct FoldBuilder {
+    auto operator()(const auto& initial, const auto& func) const {
+        return Fold(initial, func);
+    }
+};
+
+template <typename Init, typename Func> struct RunningFold {
+    RunningFold(const Init& _init, const Func& _func)
+        : init(_init),
+          func(_func) {}
+    const Init init;
+    const Func func;
+};
+
+struct RunningFoldBuilder {
+    auto operator()(const auto& initial, const auto& func) const {
+        return RunningFold(initial, func);
+    }
+};
+
+template <typename Func> struct Reduce {
+    Reduce(const Func& _func) : func(_func) {}
+    const Func func;
+};
+
+struct ReduceBuilder {
+    auto operator()(const auto& func) const {
+        return Reduce(func);
     }
 };
 
@@ -46,9 +84,48 @@ auto operator|(const std::vector<T>& vec, const Filter<Func>& filter) {
     return ret;
 }
 
+template <typename T, typename Init, typename Func>
+auto operator|(const std::vector<T>& vec, const Fold<Init, Func>& fold) {
+    auto ret = fold.init;
+    for (const auto& i : vec) {
+        ret = fold.func(ret, i);
+    }
+    return ret;
+}
+
+template <typename T, typename Init, typename Func>
+auto operator|(
+    const std::vector<T>& vec, const RunningFold<Init, Func>& running_fold
+) {
+    using Ret = decltype(running_fold.func(Init(), T()));
+    std::vector<Ret> ret{running_fold.init};
+    for (const auto& i : vec) {
+        ret.push_back(running_fold.func(ret.back(), i));
+    }
+    return ret;
+}
+
+template <typename T, typename Func>
+auto operator|(const std::vector<T>& vec, const Reduce<Func>& reduce) {
+    auto ret = vec[0];
+    for (usize i = 1; i < size(vec); ++i) {
+        ret = reduce.func(ret, vec[i]);
+    }
+    return ret;
+}
+
 } // namespace Functional
 
 inline constexpr auto filter = Functional::FilterBuilder();
+inline constexpr auto fold = Functional::FoldBuilder();
+inline constexpr auto running_fold = Functional::RunningFoldBuilder();
+inline constexpr auto reduce = Functional::ReduceBuilder();
+
+template <typename T> std::vector<T> int_range(const T lo, const T hi) {
+    std::vector<T> ret(hi - lo + 1);
+    std::iota(std::begin(ret), std::end(ret), lo);
+    return ret;
+}
 
 template <typename T>
 auto pipe_vec(const std::vector<T>& vec, const auto&... funcs) {
