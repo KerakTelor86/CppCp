b540b632c3d6d25975f1551cb7bb353c79e8a9d8
Richard Alison <richardalison86@gmail.com>
2024-06-15 16:55:23 +0900

    Add more functional constructs

--- func.hpp
+++ func.hpp
@@ -1,62 +1,112 @@
 #ifndef CPPCP_FUNCTIONAL
 #define CPPCP_FUNCTIONAL
 
+#include <algorithm>
 #include <array>
+#include <map>
 #include <numeric>
 #include <vector>
 
 #include "concepts.hpp"
 #include "types.hpp"
+#include "unordered.hpp"
+#include "zip.hpp"
 
 namespace CppCp {
+
+template <typename R = usize, typename T>
+std::vector<R> index_range_of(const std::vector<T>& vec);
 
 namespace Functional {
 
 template <typename Func> struct Filter {
-    Filter(const Func& _func) : func(_func) {}
+    constexpr Filter(const Func& _func) : func(_func) {}
     const Func func;
 };
 
-struct FilterBuilder {
-    auto operator()(const auto& func) const {
-        return Filter(func);
-    }
+template <typename Func> struct Partition {
+    constexpr Partition(const Func& _func) : func(_func) {}
+    const Func func;
+};
+
+template <typename Func> struct GroupBy {
+    constexpr GroupBy(const Func& _func) : func(_func) {}
+    const Func func;
 };
 
 template <typename Init, typename Func> struct Fold {
-    Fold(const Init& _init, const Func& _func) : init(_init), func(_func) {}
-    const Init init;
-    const Func func;
-};
-
-struct FoldBuilder {
-    auto operator()(const auto& initial, const auto& func) const {
-        return Fold(initial, func);
-    }
-};
-
-template <typename Init, typename Func> struct RunningFold {
-    RunningFold(const Init& _init, const Func& _func)
+    constexpr Fold(const Init& _init, const Func& _func)
         : init(_init),
           func(_func) {}
     const Init init;
     const Func func;
 };
 
+template <typename Init, typename Func> struct RunningFold {
+    constexpr RunningFold(const Init& _init, const Func& _func)
+        : init(_init),
+          func(_func) {}
+    const Init init;
+    const Func func;
+};
+
+template <typename Func> struct Reduce {
+    constexpr Reduce(const Func& _func) : func(_func) {}
+    const Func func;
+};
+
+template <typename Cmp> struct Sorted {
+    constexpr Sorted() {}
+};
+
+struct Unique {};
+
+struct WithIndex {};
+
+struct FilterBuilder {
+    constexpr auto operator()(const auto& func) const {
+        return Filter(func);
+    }
+};
+
+struct PartitionBuilder {
+    constexpr auto operator()(const auto& func) const {
+        return Partition(func);
+    }
+};
+
+struct GroupByBuilder {
+    constexpr auto operator()(const auto& func) const {
+        return GroupBy(func);
+    }
+};
+
+struct FoldBuilder {
+    constexpr auto operator()(const auto& initial, const auto& func) const {
+        return Fold(initial, func);
+    }
+};
+
 struct RunningFoldBuilder {
-    auto operator()(const auto& initial, const auto& func) const {
+    constexpr auto operator()(const auto& initial, const auto& func) const {
         return RunningFold(initial, func);
     }
 };
 
-template <typename Func> struct Reduce {
-    Reduce(const Func& _func) : func(_func) {}
-    const Func func;
+struct ReduceBuilder {
+    constexpr auto operator()(const auto& func) const {
+        return Reduce(func);
+    }
 };
 
-struct ReduceBuilder {
-    auto operator()(const auto& func) const {
-        return Reduce(func);
+struct SortedBuilder {
+    constexpr auto operator()() const {
+        return Sorted<std::less<>>();
+    }
+
+    template <typename Cmp>
+    constexpr auto operator()([[maybe_unused]] const Cmp& compare) const {
+        return Sorted<Cmp>();
     }
 };
 
@@ -73,6 +123,7 @@ auto operator|(const std::vector<T>& vec, const Func& func) {
 }
 
 template <typename T, typename Func>
+    requires LambdaWithRet<bool, Func, T>
 auto operator|(const std::vector<T>& vec, const Filter<Func>& filter) {
     std::vector<T> ret;
     ret.reserve(std::ssize(vec));
@@ -82,6 +133,40 @@ auto operator|(const std::vector<T>& vec, const Filter<Func>& filter) {
         }
     }
     return ret;
+}
+
+template <typename T, typename Func>
+    requires LambdaWithRet<bool, Func, T>
+auto operator|(const std::vector<T>& vec, const Partition<Func>& partition) {
+    std::pair<std::vector<T>, std::vector<T>> ret;
+    for (const auto& i : vec) {
+        if (partition.func(i)) {
+            ret.first.push_back(i);
+        } else {
+            ret.second.push_back(i);
+        }
+    }
+    return ret;
+}
+
+template <typename T, typename Func>
+auto operator|(const std::vector<T>& vec, const GroupBy<Func>& group_by) {
+    using Key = decltype(group_by.func(T()));
+    if constexpr (UnorderedHashable<Key>) {
+        UnorderedMap<Key, std::vector<T>> groups;
+        for (const auto& i : vec) {
+            const auto key = group_by.func(i);
+            groups[key].push_back(i);
+        }
+        return groups;
+    } else {
+        std::map<Key, std::vector<T>> groups;
+        for (const auto& i : vec) {
+            const auto key = group_by.func(i);
+            groups[key].push_back(i);
+        }
+        return groups;
+    }
 }
 
 template <typename T, typename Init, typename Func>
@@ -116,17 +201,64 @@ auto operator|(const std::vector<T>& vec, const Reduce<Func>& reduce) {
     return ret;
 }
 
+template <typename T, typename Cmp>
+auto operator|(
+    const std::vector<T>& vec, [[maybe_unused]] const Sorted<Cmp>& sorted
+) {
+    auto ret = vec;
+    std::sort(std::begin(ret), std::end(ret));
+    return ret;
+}
+
+template <typename T>
+auto operator|(
+    const std::vector<T>& vec, [[maybe_unused]] const Unique& unique
+) {
+    auto ret = vec;
+    std::sort(std::begin(ret), std::end(ret));
+    ret.erase(std::unique(std::begin(ret), std::end(ret)), std::end(ret));
+    return ret;
+}
+
+template <typename T>
+auto operator|(
+    const std::vector<T>& vec, [[maybe_unused]] const WithIndex& with_index
+) {
+    return zip(index_range_of(vec), vec);
+}
+
 } // namespace Functional
 
 inline constexpr auto filter = Functional::FilterBuilder();
+inline constexpr auto partition_by = Functional::PartitionBuilder();
+inline constexpr auto group_by = Functional::GroupByBuilder();
 inline constexpr auto fold = Functional::FoldBuilder();
 inline constexpr auto running_fold = Functional::RunningFoldBuilder();
 inline constexpr auto reduce = Functional::ReduceBuilder();
+inline constexpr auto to_sorted_with = Functional::SortedBuilder();
+inline constexpr auto to_sorted_desc = to_sorted_with(std::greater<>());
+inline constexpr auto to_sorted = to_sorted_with();
+inline constexpr auto to_unique = Functional::Unique();
+inline constexpr auto with_index = Functional::WithIndex();
 
 template <typename T> std::vector<T> int_range(const T lo, const T hi) {
+    debug_assert(lo <= hi, "cannot create an empty range");
     std::vector<T> ret(hi - lo + 1);
     std::iota(std::begin(ret), std::end(ret), lo);
     return ret;
+}
+
+template <typename R, typename T>
+std::vector<R> index_range_of(const std::vector<T>& vec) {
+    if (ssize(vec) == 0) {
+        return {};
+    }
+    return int_range<R>(0, ssize(vec) - 1);
+}
+
+template <typename T>
+std::vector<i32> index_range_of_i32(const std::vector<T>& vec) {
+    return index_range_of<i32>(vec);
 }
 
 template <typename T>
--- zip.hpp
+++ zip.hpp
@@ -20,10 +20,18 @@ void unzip_helper(T& vec_tuple, const U& row, std::index_sequence<Idx...>) {
     ((std::get<Idx>(vec_tuple).push_back(std::get<Idx>(row))), ...);
 }
 
+template <typename T, typename U, usize... Idx>
+void zip_helper(const T& vec_tuple, U& row, usize row_idx, std::index_sequence<Idx...>) {
+    debug_assert(
+        ((row_idx < std::size(std::get<Idx>(vec_tuple))) && ...),
+        "size not enough to zip (less than size of first vector in the tuple)"
+    );
+    ((std::get<Idx>(row) = std::get<Idx>(vec_tuple)[row_idx]), ...);
+}
+
 } // namespace
 
-template <typename... T>
-auto unzip(const std::vector<std::tuple<T...>>& vec) {
+template <typename... T> auto unzip(const std::vector<std::tuple<T...>>& vec) {
     std::tuple<std::vector<T>...> ret;
 
     reserve_helper(ret, size(vec), std::index_sequence_for<T...>{});
@@ -32,6 +40,27 @@ auto unzip(const std::vector<std::tuple<T...>>& vec) {
     }
 
     return ret;
+}
+
+template <typename T, typename... Rest>
+auto zip(const std::tuple<std::vector<T>, std::vector<Rest>...>& tup) {
+    const auto size = std::size(std::get<0>(tup));
+    using Row = std::tuple<T, Rest...>;
+
+    std::vector<Row> ret;
+    ret.reserve(size);
+
+    for (usize i = 0; i < size; ++i) {
+        auto row = Row();
+        zip_helper(tup, row, i, std::index_sequence_for<T, Rest...>{});
+        ret.push_back(std::move(row));
+    }
+
+    return ret;
+}
+
+template <typename... T> auto zip(const std::vector<T>... vecs) {
+    return zip(make_tuple(vecs...));
 }
 
 } // namespace CppCp
